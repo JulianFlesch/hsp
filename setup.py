f82ac9f08966599ed7e3885defac45578ea22777
julian <julianflesch@gmail.com>
2019-10-25 17:31:02 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hsp",
-    version="0.1.2",
+    version="0.1.3",
     author="Julian Flesch",
     author_email="julianflesch@gmail.com",
     keywords="hochschulsport uni Tübingen",
