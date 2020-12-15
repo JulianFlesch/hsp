abf3b658795ac513d6da4c5f65a07c5eed1d81bd
julian <julianflesch@gmail.com>
2020-12-15 16:57:28 +0100

    fixes a bug, where no surname is entered

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hsp",
-    version="1.0.4",
+    version="1.0.5",
     author="Julian Flesch",
     author_email="julianflesch@gmail.com",
     keywords="hochschulsport uni Tübingen",
