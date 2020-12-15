aace5c4d1ea0f842ebe21d96d591811608b5ecc0
julian <julianflesch@gmail.com>
2020-12-15 16:29:43 +0100

    Fixes error due to renamed attribute names.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hsp",
-    version="1.0.2",
+    version="1.0.3",
     author="Julian Flesch",
     author_email="julianflesch@gmail.com",
     keywords="hochschulsport uni Tübingen",
